6130021d0168bdc58548e26c2615e7ce376cce5c
seankurt12 <seanvkurt.01@gmail.com>
2025-07-20 16:02:40 +0800

    SharedPrefsManager.kt

--- app/src/main/java/com/example/jdm_gacha_simulator/utils/SharedPrefsManager.kt
+++ app/src/main/java/com/example/jdm_gacha_simulator/utils/SharedPrefsManager.kt
@@ -1,4 +1,22 @@
 package com.example.jdm_gacha_simulator.utils
 
-class SharedPrefsManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPrefsManager(context: Context) {
+
+    private val sharedPrefs: SharedPreferences =
+        context.getSharedPreferences(Constants.PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun saveUserId(userId: String) {
+        sharedPrefs.edit().putString(Constants.KEY_USER_ID, userId).apply()
+    }
+
+    fun getUserId(): String? {
+        return sharedPrefs.getString(Constants.KEY_USER_ID, null)
+    }
+
+    fun clearUserId() {
+        sharedPrefs.edit().remove(Constants.KEY_USER_ID).apply()
+    }
 }
